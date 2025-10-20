281840a7159937d704f79fa87696c9aaab4ecf97
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-10-20 12:27:34 +0200

    Fixes

--- scripts/shared.ts
+++ scripts/shared.ts
@@ -1,7 +1,8 @@
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export const HERE = import.meta.url.startsWith('file:')
-  ? new URL('.', import.meta.url).pathname
-  : process.cwd();
+const __filename = fileURLToPath(import.meta.url);
+export const HERE = dirname(__filename);
+
 export const DOWNLOAD_PATH = join(HERE, '../static/assets/');
 export const TRANSLATIONS_FILES_DIR = join(HERE, '../src/lib/i18n/messages');
