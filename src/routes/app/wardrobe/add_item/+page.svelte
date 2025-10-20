2f4af5ebb571d06d4ad45139a541b22265cbbd1a
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-10-20 09:20:10 +0200

    UI and UX improvements

--- src/routes/app/wardrobe/add_item/+page.svelte
+++ src/routes/app/wardrobe/add_item/+page.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
   import { Toaster } from '$lib/components/Toast/toast';
+  import Input from '$lib/components/ui/input/input.svelte';
+  import * as Select from '$lib/components/ui/select';
   import { t } from '$lib/i18n';
-  import { clothingItemTypes } from '$lib/types';
+  import {
+    clothingItemColors,
+    clothingItemTypes,
+    type ClothingItemColor,
+    type ClothingItemType,
+  } from '$lib/types';
   import { logger } from '$lib/utils/logger';
   import type { PageProps } from './$types';
+  import * as Field from '$lib/components/ui/field/index.js';
+  import { Button } from '$lib/components/ui/button';
 
   let { form }: PageProps = $props();
+  let color = $state<ClothingItemColor>(clothingItemColors[0]);
+  let type = $state<ClothingItemType>(clothingItemTypes[0]);
 
   $effect(() => {
     if (form?.message && form?.action === 'create_item') {
@@ -23,33 +34,61 @@
       }
     }
   });
+
+  const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 </script>
 
-<form
-  action="?/create_item"
-  method="POST"
-  class="flex flex-col"
-  use:enhance
-  enctype="multipart/form-data"
->
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name" />
+<div class="mx-auto w-full max-w-[650px] p-2">
+  <form
+    action="?/create_item"
+    method="POST"
+    class="border-border flex flex-col gap-4 rounded-lg border p-4"
+    use:enhance
+    enctype="multipart/form-data"
+  >
+    <h1 class="text-2xl font-bold">Add an item</h1>
+    <hr />
+    <Field.Field>
+      <Field.Label for="name">Name</Field.Label>
+      <Input id="name" name="name" type="text" />
+    </Field.Field>
 
-  <label for="description">Description:</label>
-  <textarea id="description" name="description"></textarea>
+    <Field.Field>
+      <Field.Label for="description">Description</Field.Label>
+      <Input id="description" name="description" type="text" />
+    </Field.Field>
 
-  <label for="type">Type:</label>
-  <select name="type" id="type">
-    {#each clothingItemTypes as type}
-      <option>{type}</option>
-    {/each}
-  </select>
+    <div class="grid grid-cols-2 gap-2">
+      <Field.Field>
+        <Field.Label for="type">Type</Field.Label>
+        <Select.Root type="single" name="type" bind:value={type}>
+          <Select.Trigger>{capitalize(type)}</Select.Trigger>
+          <Select.Content>
+            {#each clothingItemTypes as type}
+              <Select.Item value={type} label={capitalize(type)} />
+            {/each}
+          </Select.Content>
+        </Select.Root>
+      </Field.Field>
 
-  <label for="color">Color:</label>
-  <input type="text" id="color" name="color" />
+      <Field.Field>
+        <Field.Label for="color">Color</Field.Label>
+        <Select.Root type="single" name="color" bind:value={color}>
+          <Select.Trigger>{capitalize(color)}</Select.Trigger>
+          <Select.Content>
+            {#each clothingItemColors as color}
+              <Select.Item value={color} label={capitalize(color)} />
+            {/each}
+          </Select.Content>
+        </Select.Root>
+      </Field.Field>
+    </div>
 
-  <label for="image">Image:</label>
-  <input type="file" id="image" name="image" accept="image/*" />
+    <Field.Field>
+      <Field.Label for="image">Image</Field.Label>
+      <Input id="image" name="image" type="file" accept="image/*" />
+    </Field.Field>
 
-  <button type="submit">Add Item</button>
-</form>
+    <Button type="submit">Add Item</Button>
+  </form>
+</div>
