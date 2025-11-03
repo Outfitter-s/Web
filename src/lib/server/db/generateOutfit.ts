d19fbae803617b86c834e58085aefdedaecde60a
Masterking <reverbel.cle@gmail.com>
2025-11-03 12:02:26 +0100

    get weather (no UI fallback)

--- src/lib/server/db/generateOutfit.ts
+++ src/lib/server/db/generateOutfit.ts
@@ -1,10 +1,14 @@
 import type { ClothingItem, UUID } from '$lib/types';
 import { ClothingItemDAO } from './clotingItem';
+import { getWeather } from '$lib/utils/weather';
+import { clothingItemColors, clothingItemTypes } from '$lib/types';
 
 export async function generateOutfit(userId: UUID): Promise<ClothingItem[]> {
     const items = await ClothingItemDAO.getClothingItemsByUserId(userId);
 
-    //Ajout de la logique de génération de tenue ici
+    const weather = await getWeather();
+    
+    //Ajout de la logique de génération de tenue ici  
 
     return items;
 }
