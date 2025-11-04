58664a5e9ab731161aa28b20f9aa1450bb0509aa
Neo <neoetichiro@gmail.com>
2025-11-04 09:17:01 +0100

    Moving fetch outfit to swiper cards

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -4,27 +4,8 @@
   import Timeline from '$lib/components/routes/app/Timeline.svelte';
   import { t } from '$lib/i18n';
   import type { SwiperCard } from '$lib/types';
-  import { onMount } from 'svelte';
 
   let chosenOutfit = $state<SwiperCard | null>(null);
-
-  async function fetchOutfit() {
-    const res = await fetch('/api/generate-outfit', {
-      method: 'POST',
-    });
-    const outfit = await res.json();
-    console.log(outfit); // Affiche l'outfit généré
-  }
-
-  onMount(() => {
-    function handleKey(event: KeyboardEvent) {
-      if (event.key === 'o') {
-        fetchOutfit();
-      }
-    }
-    window.addEventListener('keydown', handleKey);
-    return () => window.removeEventListener('keydown', handleKey);
-  });
 </script>
 
 <SEO title={$t('seo.homePage.title')} />
