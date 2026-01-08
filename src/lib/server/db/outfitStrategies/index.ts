da8ab5f0b6c16186b88bf706bc6e45fc91f63529
ClementReverbel <reverbel.cle@gmail.com>
2026-01-08 21:40:33 +0100

    add pattern scoring for clothing item

--- src/lib/server/db/outfitStrategies/index.ts
+++ src/lib/server/db/outfitStrategies/index.ts
@@ -2,6 +2,7 @@ export { monochromeScore } from './monochrome';
 export { analogousScore } from './analogous';
 export { complementaryScore } from './complementary';
 export { triadicScore } from './triadic';
+export { patternScore } from './pattern';
 export { lastWornScore } from './lastWorn';
 export { colorForUVScore } from './colorForUV';
 export { tempScore } from './temp';
